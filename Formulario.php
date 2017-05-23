0ef219a3ca156ab834f4dacc971ac1370e93b6a3
cetecom <cetecom@LC08L5728>
2017-05-23 11:12:20 -0400

    Ejercicio

---
+++ Formulario.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form method="post" action="guardar.php">
+            <div>nombre: <input type="text" name="nombre"></div>
+            <div>clave: <input type="text" name="clave"></div>
+            <input type="submit" value="enviar">
+                
+        </form>
+    </body>
+</html>
--- lib/Usuario.php
+++ lib/Usuario.php
@@ -25,7 +25,7 @@ class Usuario{
         
     }
     
-    function VerificaUsuarioClave(){
+    function ModificarUsuarioClave(){
         $oConn=new Conexion();
         
         if ($oConn->Conectar())
